9ce28e182cec191308f34f8a86180320aa383014
Chris Grochowski <cgrocho@gmail.com>
2022-01-31 22:03:20 -0800

    Test snap.svg.js in post.

---
+++ _includes/basic_snap.js
@@ -0,0 +1,3 @@
+var s = Snap(clock);
+
+var r = s.rect(100,100,100,100,20,20).attr({ stroke: '#123456', 'strokeWidth': 20, fill: 'red', 'opacity': 0.2 });
---
+++ assets/js/bundle.js
@@ -0,0 +1,3 @@
+---
+---
+{% include_relative snap.svg.js %}
