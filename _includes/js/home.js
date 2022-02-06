1f63fe39826659400684f0aa3b6366304e94d578
Chris Grochowski <cgrocho@gmail.com>
2022-02-05 22:44:03 -0800

    Added amplitude and y-offset to waves.

--- _includes/js/home.js
+++ _includes/js/home.js
@@ -1,21 +1,21 @@
 
-function createWave ( svgTag, pathTag, t0, offset_t ) {
-  var initial_path = calculatePath(t0, offset_t);
+function createWave ( svgTag, pathTag, t0, offset_t, a, offset_y ) {
+  var initial_path = calculatePath(t0, offset_t, a, offset_y);
   $(pathTag).attr("d", initial_path);
 
-  myLoop(initial_path, second, 1, svgTag, pathTag, offset_t);
+  myLoop(initial_path, second, 1, svgTag, pathTag, offset_t, a, offset_y);
 };
 
 
 
 
 
-function calculatePath (t, offset_t) {
+function calculatePath (t, offset_t, a, offset_y) {
   var seconds = [...Array(100).keys()];
   var my_points = '';
   var i = 1;
   for (const s of seconds) {
-    var raw_s = 10 * Math.sin(Math.sin(t/6)*Math.sin(t/6)*s*0.06 + offset_t*t) + 20
+    var raw_s = a * Math.sin(Math.sin(t/6)*Math.sin(t/6)*s*0.06 + offset_t*t) + offset_y
     my_points = my_points + i + ',' + raw_s + ' '
     i = i + 1;
   };
@@ -28,23 +28,23 @@ function calculatePath (t, offset_t) {
 };
 
 
-function updatePath ( x0, svgTag, pathTag, offset_t ) {
+function updatePath ( x0, svgTag, pathTag, offset_t, a, offset_y ) {
   console.log(svgTag + ' - ' + pathTag + ': ' + x0);
-  var new_export_points = calculatePath(x0, offset_t);
+  var new_export_points = calculatePath(x0, offset_t, a, offset_y);
   morphToNew(new_export_points, svgTag, pathTag);
   var x1 = x0 + 0.1;
   return [new_export_points, x1];
 };
 
-function myLoop( path, x0, i, svgTag, pathTag, offset_t ) {         //  create a loop function
+function myLoop( path, x0, i, svgTag, pathTag, offset_t, a, offset_y ) {         //  create a loop function
   setTimeout(function() {   
     var oldPath = path;
-    var updatedPath = updatePath(x0, svgTag, pathTag, offset_t);             
+    var updatedPath = updatePath(x0, svgTag, pathTag, offset_t, a, offset_y);             
     oldPath = updatedPath[0];
     var x1 = updatedPath[1];
     i++;                    
     if (i > 0) {           
-      myLoop(path, x1, i, svgTag, pathTag, offset_t);           
+      myLoop(path, x1, i, svgTag, pathTag, offset_t, a, offset_y);           
     }                   
   }, 100)
   return i;
@@ -55,15 +55,15 @@ var second = new Date().getTime();
 second = String(second);
 second = second.substring(second.length-1)/10;
 
-createWave('#wave-svg', '#final-path', second, 1.1);
+createWave('#wave-svg', '#final-path', second, 1.1, 5, 30);
 
 second = String(second + 100);
 second = second.substring(second.length-1)/10;
 
-createWave('#wave-svg', '#final-path-2', second, 1);
+createWave('#wave-svg', '#final-path-2', second, 1, 8, 15);
 
 second = String(second + 100);
 second = second.substring(second.length-1)/10;
 
-createWave('#wave-svg', '#final-path-3', second, 0.6);
-createWave('#wave-svg', '#final-path-4', second, 1.2);
+createWave('#wave-svg', '#final-path-3', second, 0.6, 10, 16);
+createWave('#wave-svg', '#final-path-4', second, 1.2, 11, 20);
